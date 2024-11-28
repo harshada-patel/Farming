350bcc76f38031b9320cbce1f1cac3bb1470236a
harshada-patel <136051597+harshada-patel@users.noreply.github.com>
2024-11-28 07:29:25 +0530

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+        <title>
+            Food Order Website- Hom Page</title>
+            <link rel="stylesheet" href="../css/admin.css">
+        
+</head>
+<body>
+    <!-- menu section starts -->
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Admin</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Food</a></li>
+                <li><a href="#">Orde</a></li>
+                <ul>
+</div>
+</div>
+    <!-- menu section ends -->
+
+    <!-- menu content section starts -->
+    <div class="main-content">
+    <div class="wrapper">
+            Menu Content Goes Here
+</div>
+</div>
+
+    <!-- menu content section ends -->
+
+    <!-- footer section starts -->
+    <div class="footer">
+    <div class="wrapper">
+            <p class="text-center">2023 all rights reserved ,FoodForPreasure.developed by-  <a href="#" >Khushi Patel</a></p>
+</div>
+</div>
+
+    <!-- footer section ends -->
+</body>
+</html>
